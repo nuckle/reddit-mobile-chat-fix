046c0877f139e844656b46220615918dbf40ea12
Nuckle <coil@itscat.org>
2024-11-25 19:03:22 +0300

    Update manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -28,7 +28,7 @@ const sharedManifest: Partial<chrome.runtime.ManifestBase> = {
 		page: 'src/entries/options/index.html',
 		open_in_tab: true,
 	},
-	permissions: ['activeTab', 'storage', 'scripting', domain],
+	permissions: ['activeTab', 'storage', 'scripting'],
 	browser_specific_settings: {
 		gecko: {
 			id: 'addon@example.com',
@@ -39,13 +39,8 @@ const sharedManifest: Partial<chrome.runtime.ManifestBase> = {
 
 const browserAction = {
 	default_icon: {
-		16: 'icons/16.png',
-		19: 'icons/19.png',
 		32: 'icons/32.png',
-		38: 'icons/38.png',
 		48: 'icons/48.png',
-		64: 'icons/64.png',
-		96: 'icons/96.png',
 		128: 'icons/128.png',
 		256: 'icons/256.png',
 		512: 'icons/512.png',
