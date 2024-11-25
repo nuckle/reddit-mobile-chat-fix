4d9b9b7022133360e2ff6deebfdfdc6554270041
Nuckle <coil@itscat.org>
2024-11-25 22:03:06 +0300

    Delete old utils.ts

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const getAppDomain = (): string => {
-	return '*://chat.reddit.com/*';
-};
-
-export const getUserAgent = (): string => {
-	return 'Mozilla/5.0 (Linux; Android 10; RMX2151) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.86 Mobile Safari/537.36';
-};
