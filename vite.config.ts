a515f81e93ba35729fe693ea1578c7043efe8675
Nuckle <coil@itscat.org>
2024-11-25 22:03:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig, loadEnv } from "vite";
-import webExtension from "@samrum/vite-plugin-web-extension";
-import path from "path";
-import { getManifest } from "./src/manifest";
+import { defineConfig, loadEnv } from 'vite';
+import webExtension from '@samrum/vite-plugin-web-extension';
+import path from 'path';
+import { getManifest } from './src/manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+	const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [
-      webExtension({
-        manifest: getManifest(Number(env.MANIFEST_VERSION)),
-        additionalInputs: {
-          scripts: ['src/entries/injectChat.js']
-        }
-      }),
-    ],
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
+	return {
+		plugins: [
+			webExtension({
+				manifest: getManifest(Number(env.MANIFEST_VERSION)),
+				additionalInputs: {
+					scripts: ['src/entries/injectChat.js'],
+				},
+			}),
+		],
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	};
 });
